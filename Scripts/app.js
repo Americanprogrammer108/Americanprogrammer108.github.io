c6543e9d5653ffb1bca2ec7f72be562c391000d9
Americanprogrammer108 <119545517+Americanprogrammer108@users.noreply.github.com>
2023-01-31 22:07:48 -0500

    Updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -33,6 +33,24 @@
 
     function DisplayHomePage()
     {
+        let MainContent = document.getElementsByTagName("main")[0];
+        let MainParagraph = document.createElement("p")
+
+        MainParagraph.setAttribute("id", "MainParagraph")
+        MainParagraph.setAttribute("class", "mt-3")
+        MainParagraph.textContent = "This is the main paragraph"
+        MainContent.appendChild(MainParagraph);
+
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the main paragraph`;
+        MainParagraph.textContent = SecondString;
+
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+        Article.setAttribute("class", "container")
+        Article.innerHTML = ArticleParagraph;
+        MainContent.appendChild(Article);
+
         let AboutUsButton = document.getElementById("AboutUsBtn");
         AboutUsButton.addEventListener("click", function ()
         {
