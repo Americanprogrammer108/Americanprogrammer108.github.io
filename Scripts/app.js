5f47e6bca3a077a0a68c29e4d0ff4d56b6217765
Americanprogrammer108 <119545517+Americanprogrammer108@users.noreply.github.com>
2023-02-03 20:20:34 -0500

    Updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,6 +5,16 @@
 
 (function (){
 
+    function addContact(fullName, contactNumber, emailAddress)
+    {
+        let contact = new core.Contact();
+        if (contact.serialize())
+        {
+            let key = contact.FullName.substring(0, 1) + Date.now();
+            localStorage.setItem(key, contact.serialize());
+        }
+    }
+
     function Start()
     {
         console.log("App started")
@@ -25,6 +35,12 @@
             case "Contact Us":
                 DisplayContactPage();
                 break;
+            case "Contact List":
+                DisplayContactListPage();
+                break;
+            case "Edit Contact":
+                DisplayEditContactPage();
+                break;
 
         }
 
@@ -33,47 +49,164 @@
 
     function DisplayHomePage()
     {
+        console.log("Home page called");
+
+        $("#AboutUsBtn").on("click", () => {
+            location.href = "about.html"
+        });
+
+        $("main").append(`<p id="MainParagraph" class="mt-3">This is my main paragraph</p>`);
+        $("body").append(`<article class="container">
+                        <p id="ArticleParagraph" class="mt-3">This is my article paragraph</p></article>`);
+
         let MainContent = document.getElementsByTagName("main")[0];
         let MainParagraph = document.createElement("p")
-
         MainParagraph.setAttribute("id", "MainParagraph")
         MainParagraph.setAttribute("class", "mt-3")
         MainParagraph.textContent = "This is the main paragraph"
-        MainContent.appendChild(MainParagraph);
 
+        MainContent.appendChild(MainParagraph);
         let FirstString = "This is";
-        let SecondString = `${FirstString} the main paragraph`;
+        let SecondString = `${FirstString} the Main Paragraph`;
         MainParagraph.textContent = SecondString;
 
         let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p> `
         Article.setAttribute("class", "container")
-        Article.innerHTML = ArticleParagraph;
-        MainContent.appendChild(Article);
 
-        let AboutUsButton = document.getElementById("AboutUsBtn");
-        AboutUsButton.addEventListener("click", function ()
-        {
-            location.href = "about.html"
-        })
     }
 
     function DisplayProductsPage()
     {
-
+        console.log("Products Page");
     }
     function DisplayServicesPage()
     {
-
+        console.log("Services Page");
     }
     function DisplayContactPage()
     {
+        console.log("Contact Page");
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckBox = document.getElementById("subscribeCheckbox");
 
+        sendButton.addEventListener("click", function (event)
+        {
+            if(subscribeCheckBox.checked)
+            {
+                let contact = new Contact(fullname.value, contactnumber.value, EmailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        console.log("Contact List Page");
+        $("#addButton").on("click", () => {
+            location.href = "edit.html#add"
+        });
+
+        if(localStorage.length > 0)
+        {
+            let ContactList = document.getElementById("contactList");
+            let data = "";
+
+            let keys = Object.keys(localStorage);
+
+            let index = 1;
+            for (const key of keys)
+            {
+                let contactData = localStorage.getItem(key);
+                let contact = new Contact();
+                contact.deserialize(contactData);
+                data += `<tr><th scope="row" class="text-center">${index}</th>
+                         <td>${contact.fullName}</td>
+                         <td>${contact.contactNumber}</td>
+                         <td>${contact.EmailAddress}</td>
+                         <td>
+                             <button value="${key}" class="btn btn-primary btn-sm mb-1 edit">
+                                <i class="fas fa-edit fa-sm">Edit</i>
+                             </button>
+                         </td>
+                         <td>
+                         <button value="${key}" class="btn btn-danger btn-sm mb-1 delete">
+                            <i class="fas fa-edit fa-sm">Delete</i>
+                         </button></td>
+                         </tr>`;
+                index++;
+            }
+            ContactList.innerHTML = data;
+
+            $("#addButton").on("click", () => {
+                location.href = "edit.html#add"
+            });
+
+            $("button.delete").on("click", function () {
+                if(confirm("Delete contact, are you sure?"))
+                {
+                    localStorage.removeItem($(this).val())
+                }
+                location.href = "contact-list.html";
+            });
+
+            $("button.edit").on("click", function () {
+                location.href = "edit.html#" + $(this).val();
+            });
+        }
     }
     function DisplayAboutPage()
     {
-
+        console.log("About Us Page");
     }
 
+    function DisplayEditContactPage()
+    {
+        let page = location.hash.substring(1);
+        console.log("Edit page");
+        switch(page)
+        {
+            case "add":
+                $("main>h1").text("Add Contact");
+                $("#editButton").html(`<i class="fas fa-plus-circle fa-sm></i> Add`);
+                $("#editButton").on("click", (event) => {
+                    event.preventDefault();
+                    addContact(fullname.value, contactnumber.value, EmailAddress.value);
+                    location.href = "contact-list.html"
+                });
+                $("#cancelButton").on("click", () => {
+                    location.href = "contact-list.html"
+                });
+                break;
+            default:
+            {
+                //edit separation code
+                let contact = new Contact();
+                contact.deserialize(localStorage.getItem(page));
+
+                $("#fullname").val(contact.FullName);
+                $("#contactnumber").val(contact.ContactNumber);
+                $("#emailAddress").val(contact.EmailAddress);
+
+                $("#editButton").on("click", (event) => {
+                    event.preventDefault();
+
+                    contact.FullName = $("#fullname").val();
+                    contact.ContactNumber = $("#contactnumber").val();
+                    contact.EmailAddress = $("#emailAddress").val();
+
+                    localStorage.setItem(page, contact.serialize());
+
+
+                    location.href = "contact-list.html"
+                });
+            }
+            break;
+        }
+    }
 
 })();
---
+++ Scripts/namespace.js
@@ -0,0 +1,8 @@
+"use strict";
+let core;
+
+(function (core)
+{
+
+    core.Contact = Contact;
+})(core || (core = {}));
